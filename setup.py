cf69dda5f4601d684b408d7535cd16fbf289437d
Loren Davie <code@axilent.com>
2015-05-21 09:27:33 -0400

    Adding setup, version.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup 
+except ImportError, err:
+    from distutils.core import setup
+
+from ahem import VERSION
+
+setup(
+    name='Ahem',
+    version='.'.join(map(str,VERSION)),
+    description='Simple, but rich, declarative notification framework for Django',
+    packages=['ahem'],
+    include_package_data=True,
+    license='MIT',
+    author='Loren Davie',
+    author_email='code@axilent.com',
+    url='https://github.com/Axilent/Ahem',
+    install_requires=['Django>=1.4'],
+    classifiers=[
+        'Development Status :: 1 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
