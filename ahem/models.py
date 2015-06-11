8fc4b4452adccf4a6f24390a667c15dea6962d0c
Filipe Ximenes <filipeximenes@gmail.com>
2015-06-11 14:50:19 -0300

    testing user and backend are unique together in backend registry model

--- ahem/models.py
+++ ahem/models.py
@@ -4,6 +4,7 @@ Models for ahem.
 import json
 
 from django.db import models
+from django.conf import settings
 
 from ahem.dispatcher import register_notifications
 
@@ -12,38 +13,38 @@ class UserBackendRegistry(models.Model):
     """
     Availble backends for a given recipient
     """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='backends')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='ahem_backends')
     backend = models.CharField(max_length=255)
 
     settings = models.TextField(blank=True) # JSON serialized user settings
     
     class Meta:
-        unique_together = (('recipient','backend'),)
+        unique_together = (('user','backend'),)
 
 
-class DeferredNotification(models.Model):
-    """
-    A notification that has been deferred until a future time.
-    """
-    notification = models.CharField(max_length=255)
-    context = models.TextField(blank=True) # JSON serialized notification context
+# class DeferredNotification(models.Model):
+#     """
+#     A notification that has been deferred until a future time.
+#     """
+#     notification = models.CharField(max_length=255)
+#     context = models.TextField(blank=True) # JSON serialized notification context
 
-    created = models.DateTimeField(auto_now=True)
+#     created = models.DateTimeField(auto_now=True)
 
 
-class SentNotification(models.Model):
-    """
-    A notification that has been sent.
-    """
-    user_backend = models.ForeignKey(UserBackendRegistry)
-    notification = models.CharField(max_length=255)
-    context = models.TextField(blank=True)
-    body = models.TextField(blank=True)
+# class SentNotification(models.Model):
+#     """
+#     A notification that has been sent.
+#     """
+#     user_backend = models.ForeignKey(UserBackendRegistry)
+#     notification = models.CharField(max_length=255)
+#     context = models.TextField(blank=True)
+#     body = models.TextField(blank=True)
 
-    created = models.DateTimeField(auto_now=True)
+#     created = models.DateTimeField(auto_now=True)
     
 
 # ==========================
 # = Main hook for registry =
 # ==========================
-register_notifications()
+# register_notifications()
---
+++ testsettings.py
@@ -0,0 +1,34 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+    'ahem',
+)
+
+import django
+from distutils.version import LooseVersion
+
+if LooseVersion(django.get_version()) >= LooseVersion('1.5') \
+    and LooseVersion(django.get_version()) < LooseVersion('1.6'):
+    INSTALLED_APPS += ('discover_runner',)
+    TEST_RUNNER = 'discover_runner.DiscoverRunner'
+
+SECRET_KEY = 'abcde12345'
+
+if LooseVersion(django.get_version()) >= LooseVersion('1.7'):
+    MIDDLEWARE_CLASSES = (
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware'
+    )
