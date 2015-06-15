061c9b2950166149c3c8befc13f94068831bc52c
Filipe Ximenes <filipeximenes@gmail.com>
2015-06-15 11:44:39 -0300

    defining triggers API

--- ahem/triggers.py
+++ ahem/triggers.py
@@ -3,23 +3,30 @@
 
 class NotificationTrigger(object):
     """
-
+    Base class for notification triggers
     """
-    pass
+    def next_run_eta(self, last_run_at=None):
+        return None
 
 
-class DelayedEvent(NotificationTrigger):
+class DelayedTrigger(NotificationTrigger):
     """
-
+    A trigger that will only run one time
     """
-    def __init__(self, countdown=0, eta=None):
-        self.countdown = countdown
-        self.eta = eta
+    def __init__(self, delay_timedelta=None):
+        self.delay_timedelta = delay_timedelta
+
+    def next_run_eta(self, last_run_at=None):
+        if last_run_at is not None:
+            return None
 
 
-class CalendarSchedule(NotificationTrigger):
+class CalendarTrigger(NotificationTrigger):
     """
-
+    A trigger that schedules itself again after is run
     """
     def __init__(self, crontab):
         self.crontab = crontab
+
+    def next_run_eta(self, last_run_at=None):
+        pass
