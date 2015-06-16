f748c7b6e51f53ec9d16ea97d169adadc98b3adc
Filipe Ximenes <filipeximenes@gmail.com>
2015-06-16 17:18:08 -0300

    testing get_notification ahem utils

--- ahem/tests/test_utils.py
+++ ahem/tests/test_utils.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 
 from ahem.notification import Notification
 from ahem.dispatcher import add_to_registry, notification_registry
+from ahem.utils import get_notification
 
 
 class TestNotification(Notification):
@@ -13,5 +14,18 @@ class OtherNotification(Notification):
     name = 'other_notification'
 
 
-add_to_registry(TestNotification)
-add_to_registry(OtherNotification)
+class GetNotificationTests(TestCase):
+
+    def setUp(self):
+        add_to_registry(TestNotification)
+        add_to_registry(OtherNotification)
+
+    def test_get_test_notification(self):
+        notification = get_notification(TestNotification.name)
+
+        self.assertEqual(notification.__class__, TestNotification)
+
+    def test_get_other_notification(self):
+        notification = get_notification(OtherNotification.name)
+
+        self.assertEqual(notification.__class__, OtherNotification)
