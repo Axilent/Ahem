fe977c56cec3016f23791768c15a2d1ec1fe7fdf
Filipe Ximenes <filipeximenes@gmail.com>
2015-06-18 19:50:32 -0300

    allowing none delay_timedelta

--- ahem/triggers.py
+++ ahem/triggers.py
@@ -34,6 +34,9 @@ class DelayedTrigger(NotificationTrigger):
         self.at_minute = at_minute
 
     def next_run_eta(self, last_run_at=None):
+        if self.delay_timedelta is None:
+            return None
+
         eta = timezone.now() + self.delay_timedelta
 
         if self.at_hour is not None:
