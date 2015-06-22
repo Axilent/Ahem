cc76a9a59ed63c3c9d6961dfc5e8fe61d61c2e7b
Loren Davie <code@axilent.com>
2015-06-22 16:45:42 -0400

    Fixing trove classifier.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/Axilent/Ahem',
     install_requires=['Django>=1.4'],
     classifiers=[
-        'Development Status :: 1 - Pre-Alpha',
+        'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
